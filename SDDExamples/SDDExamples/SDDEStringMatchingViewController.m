7bb5f6f140304b2a6d53d58f489ff84efb4381ba
zhangfree <free0103@126.com>
2016-05-26 22:37:07 +0800

    Added user input origin string and pattern

--- SDDExamples/SDDExamples/SDDEStringMatchingViewController.m
+++ SDDExamples/SDDExamples/SDDEStringMatchingViewController.m
@@ -10,12 +10,13 @@
 #import "SDDI.h"
 #import "SDDEContext.h"
 
+
 @interface SDDEStringMatchingViewController ()
 {
     SDDSchedulerBuilder *_sddBuilder;
     
-    NSString *_originString;
-    NSString *_pattern;
+    __weak IBOutlet UITextField *originTxtField;
+    __weak IBOutlet UITextField *patternTxtField;
 }
 
 @end
@@ -26,13 +27,13 @@
 {
     [super viewDidLoad];
     
-    _originString = @"abcbababacacbab";
-//    _originString = @"abcbababaabcbab";
-    _pattern = @"ababaca";
-
-    
     [self setupSDDBuilder];
-    [self setupMatchState];
+}
+
+- (void)setupSDD
+{
+    NSArray *stateTable = [self createStateTable:patternTxtField.text];
+    [self setupMatchState:stateTable];
 }
 
 #pragma mark - SDDBuilder
@@ -47,62 +48,110 @@
 
 #pragma mark - MatchState
 
-- (void)setupMatchState
+- (void)setupMatchState:(NSArray *)stateTable
 {
-    
-    //ababaca
-    NSString* dsl = SDDOCLanguage
-    (
-     [Match ~[step0_]
-      [step0_]
-      [step1_a]
-      [step2_b]
-      [step3_a]
-      [step4_b]
-      [step5_a]
-      [step6_c]
-      [step7_a]
-      ]
-     
-     [step0_]     ->  [step1_a]:    a
-     [step0_]     ->  [step0_]:    b
-     [step0_]     ->  [step0_]:    c
-     
-     [step1_a]     ->  [step1_a]:    a
-     [step1_a]     ->  [step2_b]:    b
-     [step1_a]     ->  [step0_]:    c
-     
-     [step2_b]     ->  [step3_a]:    a
-     [step2_b]     ->  [step0_]:    b
-     [step2_b]     ->  [step0_]:    c
-     
-     [step3_a]     ->  [step1_a]:    a
-     [step3_a]     ->  [step4_b]:    b
-     [step3_a]     ->  [step0_]:    c
-     
-     [step4_b]     ->  [step5_a]:    a
-     [step4_b]     ->  [step0_]:    b
-     [step4_b]     ->  [step0_]:    c
-     
-     [step5_a]     ->  [step1_a]:    a
-     [step5_a]     ->  [step4_b]:    b
-     [step5_a]     ->  [step6_c]:    c
-     
-     [step6_c]     ->  [step7_a]:    a
-     [step6_c]     ->  [step0_]:    b
-     [step6_c]     ->  [step0_]:    c
-
-     );
+    NSString *dsl = @"";
+    dsl = [dsl stringByAppendingString:[self declareMultiState:stateTable]];
+    dsl = [dsl stringByAppendingString:[self initializeMultiStateTransmit:stateTable]];
 
     [_sddBuilder hostSchedulerWithContext:self dsl:dsl];
 
 }
 
+#pragma mark - ConstructDSL
+
+- (NSString *)declareMultiState:(NSArray *)stateTable
+{
+    NSString *declareDSL = @"";
+    //head
+    declareDSL = [declareDSL stringByAppendingString:[NSString stringWithFormat:@"[Match"]];
+    //start state
+    declareDSL = [declareDSL stringByAppendingString:[NSString stringWithFormat:@" ~[step_0]"]];
+    //other state
+    for (int i = 0; i < [stateTable count]; i++) {
+        declareDSL = [declareDSL stringByAppendingString:[NSString stringWithFormat:@"[step_%d]", i]];
+    }
+    //end
+    declareDSL = [declareDSL stringByAppendingString:[NSString stringWithFormat:@"]"]];
+    
+    return declareDSL;
+}
+
+- (NSString *)initializeMultiStateTransmit:(NSArray *)stateTable
+{
+    NSString *stateTransmitDSL = @"";
+    
+    for (int i = 0; i < [stateTable count] - 1; i++) {
+        NSArray *cArr = [stateTable objectAtIndex:i];
+        for (int j = 0; j < [cArr count]; j++) {
+            NSString *triggerChar = [NSString stringWithFormat:@"%c", j+97];
+            int nextState = [((NSNumber *)[cArr objectAtIndex:j]) intValue];
+            stateTransmitDSL = [stateTransmitDSL stringByAppendingString:[NSString stringWithFormat:@"[step_%d] -> [step_%d] : %@", i, nextState, triggerChar]];
+        }
+    }
+    
+    return stateTransmitDSL;
+}
+
+#pragma mark - CreateStateTable
+- (NSArray *)createStateTable:(NSString *)pattern
+{
+    NSMutableArray *stateTable = [[NSMutableArray alloc] init];
+    NSInteger len = [pattern length];
+    for (NSInteger state = 0;state <= len;state++) {
+        NSMutableArray *cArr = [[NSMutableArray alloc] init];
+        [stateTable addObject:cArr];
+        for (int c = 97; c <= 122; c++) {
+            NSString *curChar = [NSString stringWithFormat:@"%c", c];
+            NSNumber *nextState = [self getNextStateWithPattern:pattern curState:state curChar:curChar];
+            [cArr addObject:nextState];
+        }
+    }
+    
+    return stateTable;
+}
+
+- (NSNumber *)getNextStateWithPattern:(NSString *)pattern
+                             curState:(NSInteger)curState
+                                 curChar:(NSString *)curChar
+{
+    if (curState == [pattern length]) {
+        return @(0);
+    }
+    NSString *charInPattern = [pattern substringWithRange:NSMakeRange(curState, 1)];
+    if (curState < [pattern length] && [curChar isEqualToString:charInPattern])
+        return @(curState+1);
+    
+    NSInteger ns, i;
+    for (ns = curState; ns > 0; ns--)
+    {
+        if([[pattern substringWithRange:NSMakeRange(ns - 1, 1)] isEqualToString:curChar])
+        {
+            for(i = 0; i < ns-1; i++)
+            {
+                NSString *prefix = [pattern substringWithRange:NSMakeRange(i, 1)];
+                NSString *suffix = [pattern substringWithRange:NSMakeRange(curState - ns + 1 + i, 1)];
+                if (![prefix isEqualToString:suffix])
+                    break;
+            }
+            if (i == ns-1)
+                return @(ns);
+        }
+    }
+    
+    return @(0);
+}
+
 #pragma mark - Action
 - (IBAction)onClickStartSearch:(id)sender {
     
-    for (int index = 0; index < [_originString length]; index++) {
-        NSString *subStr = [_originString substringWithRange:NSMakeRange(index, 1)];
+    NSParameterAssert(originTxtField.text);
+    NSParameterAssert(patternTxtField.text);
+    
+    [self setupSDD];
+    
+    for (int index = 0; index < [originTxtField.text length]; index++) {
+        NSString *subStr = [originTxtField.text substringWithRange:NSMakeRange(index, 1)];
         [_sddBuilder.epool scheduleEvent:subStr];
     }
 }
