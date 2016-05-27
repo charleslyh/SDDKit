c58ec295b28113261b31ba7db41ac3bb4ccd0cc0
Charles <charles.liyh@gmail.com>
2016-05-27 10:46:50 +0800

    My own changes

--- SDDExamples/SDDExamples/SDDEContext.m
+++ SDDExamples/SDDExamples/SDDEContext.m
@@ -8,8 +8,25 @@
 
 #import "SDDEContext.h"
 #import <SDDI/SDDI.h>
+#import <UIKit/UIKit.h>
+
+
+@implementation NSString (Test)
+
+- (BOOL)isValid    { return NO; }
+- (BOOL)isNotEmpty { return NO; }
+
+@end
+
 
 Context* globalContext;
+
+@interface Context()
+@property (nonatomic, readonly) NSString *username;
+@property (nonatomic, readonly) NSString *password;
+@property (nonatomic) UITextField *usernameField;
+@property (nonatomic) UITextField *passwordField;
+@end
 
 @implementation Context
 -(instancetype)initWithReporter:(SDDISocketReporter *)reporter {
@@ -18,4 +35,44 @@ Context* globalContext;
     }
     return self;
 }
+
+- (void)enableSMSButton {}
+- (void)enableVerifyButton {}
+- (void)loginWithCompletion:(void (^)(BOOL, BOOL))completion {}
+- (void)showPicCodeInputControls {}
+- (void)showMessage:(NSString *)message {}
+- (void)dismiss {}
+- (BOOL)isMessageAlreadyShown { return NO; }
+
+- (IBAction)didTouchLoginButton:(id)sender {
+    if ([self.usernameField.text isValid] && [self.passwordField.text isNotEmpty]) {
+        self.usernameField.enabled = NO;
+        self.passwordField.enabled = NO;
+        
+        [self loginWithCompletion:^(BOOL authorized, BOOL needPicCodeVerify) {
+            if (authorized) {
+                if (![self isMessageAlreadyShown]) {
+                    [self showMessage:@"登录成功"];
+                }
+                
+                [self dismiss];
+            } else {
+                if (needPicCodeVerify) {
+                    if (![self isMessageAlreadyShown]) {
+                        [self showMessage:@"请进行进一步验证"];
+                    }
+                    [self showPicCodeInputControls];
+                } else {
+                    if (![self isMessageAlreadyShown]) {
+                        [self showMessage:@"用户名或密码错误"];
+                    }
+                }
+            }
+            
+            self.usernameField.enabled = YES;
+            self.passwordField.enabled = YES;
+        }];
+    }
+}
+
 @end
